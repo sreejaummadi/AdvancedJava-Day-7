527a32300e4719f6f82c5516aaae90492a6ca928
Ummadi Sreeja <157631852+sreejaummadi@users.noreply.github.com>
2025-12-16 12:04:38 +0530

    LoginServlet with servlet Communication

---
+++ FailureServlet.java
@@ -0,0 +1,24 @@
+package com.codegnan.web;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class FailureServlet
+ */
+@WebServlet("/failure")
+public class FailureServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().println("Login Failure-Invalis Credentials");
+	}
+
+	
+}
---
+++ SuccessServlet.java
@@ -0,0 +1,24 @@
+package com.codegnan.web;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class SuccessServlet
+ */
+@WebServlet("/success")
+public class SuccessServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().println("Login Success ");
+	}
+
+}
